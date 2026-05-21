8d96202b9d6585fa9638f77044f3364dfe95b4fc
Ana Clara <ana.c.silva659@aluno.senai.br>
2026-05-21 12:41:56 -0300

    Precisa estudar mais

---
+++ 21-05-26 navegacao entre telas/sesi-pet/app/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Text, View, ScrollView, TouchableOpacity, StyleSheet, Image, TextInput } from "react-native";
+
+
+
+
+export default function Index() {
+  
+  return (
+  <view>
+
+  </view>
+  );
+}
+
+
+ 
